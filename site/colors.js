18eb8215f44dadb32cec18a7c49f1f900abd3dcc
Chris Krycho <hello@chriskrycho.com>
2022-12-31 22:09:32 -0700

    Dynamically update theme-color

--- site/colors.js
+++ site/colors.js
@@ -10,6 +10,8 @@ let intervals = {
   },
 };
 
+let themeColorMetaTags = document.querySelectorAll('meta[name="theme-color"]');
+
 let mq = window.matchMedia("(prefers-reduced-motion)");
 if (!mq.matches) {
   changeColors();
@@ -20,13 +22,15 @@ mq.addEventListener("change", ({ matches }) => {
   if (matches) changeColors();
 });
 
+let r = document.querySelector(":root");
+
 function changeColors() {
-  let r = document.querySelector(":root");
   let lDir = -0.1;
   let dDir = +0.1;
   intervals.hi = setInterval(() => {
     let h = +getComputedStyle(r).getPropertyValue("--h");
     r.style.setProperty("--h", (h + 1) % 360);
+    setThemeColor();
   }, 200);
 
   setTimeout(() => {
@@ -35,6 +39,7 @@ function changeColors() {
       let nextL = l + lDir;
       lDir = nextL < 85 ? 0.1 : nextL > 95 ? -0.1 : lDir;
       r.style.setProperty("--l", nextL + "%");
+      setThemeColor(r);
     }, 400);
   }, 100);
 
@@ -45,6 +50,12 @@ function changeColors() {
       dDir = nextD < 10 ? 0.1 : nextD > 20 ? -0.1 : dDir;
 
       r.style.setProperty("--d", nextD + "%");
+      setThemeColor(r);
     }, 400);
   }, 300);
+}
+
+function setThemeColor() {
+  let newStyle = getComputedStyle(r).backgroundColor;
+  themeColorMetaTags.forEach((tag) => tag.setAttribute("content", newStyle));
 }
